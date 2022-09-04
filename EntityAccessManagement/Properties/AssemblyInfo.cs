1ddd39b01eb3d777f8bee3c6205a832cb3e3fced
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-09-04 09:42:21 +0200

    :recycle: Refactored the project :bug: Fixed #1 and #2

--- EntityAccessManagement/Properties/AssemblyInfo.cs
+++ EntityAccessManagement/Properties/AssemblyInfo.cs
@@ -5,12 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EntityAccessManagement")]
+
 [assembly: AssemblyDescription("Manage role permissions in the dimension of entity")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zhao Tan")]
-[assembly: AssemblyProduct("EntityAccessManagement")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +18,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("03d42fa1-af20-48c6-b553-b0c8716ca3bd")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2022.4.0")]
-[assembly: AssemblyFileVersion("1.2022.4.0")]
